52181b15b39264a09d05f3f17cced818d0e5be72
unknown <sebastianctsimmons@gmail.com>
2020-09-01 02:46:57 -0700

    cargo cleaned chapter 4

--- chapter_4/slice_type/target/debug/deps/slice_type.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Sebastian\rust_projects\chapter_4\slice_type\target\debug\deps\slice_type.exe: src\main.rs
-
-C:\Users\Sebastian\rust_projects\chapter_4\slice_type\target\debug\deps\slice_type.d: src\main.rs
-
-src\main.rs:
--- chapter_4/slice_type/target/debug/slice_type.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Sebastian\rust_projects\chapter_4\slice_type\target\debug\slice_type.exe: C:\Users\Sebastian\rust_projects\chapter_4\slice_type\src\main.rs
