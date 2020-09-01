abfa083e70763beb4a81533792862c14c865a8cc
unknown <sebastianctsimmons@gmail.com>
2020-09-01 02:43:14 -0700

    chapter 4

---
+++ chapter_4/slice_type/target/debug/deps/slice_type.d
@@ -0,0 +1,5 @@
+C:\Users\Sebastian\rust_projects\chapter_4\slice_type\target\debug\deps\slice_type.exe: src\main.rs
+
+C:\Users\Sebastian\rust_projects\chapter_4\slice_type\target\debug\deps\slice_type.d: src\main.rs
+
+src\main.rs:
---
+++ chapter_4/slice_type/target/debug/slice_type.d
@@ -0,0 +1,1 @@
+C:\Users\Sebastian\rust_projects\chapter_4\slice_type\target\debug\slice_type.exe: C:\Users\Sebastian\rust_projects\chapter_4\slice_type\src\main.rs
